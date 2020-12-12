45b4e45ececdd86707d6a782607b609fcc4ccd9d
PanagiotisKolokouris <72411458+PanagiotisKolokouris@users.noreply.github.com>
2020-12-12 18:52:54 +0200

    Add files via upload

---
+++ exporters/MarkdownExporter.java
@@ -0,0 +1,16 @@
+package exporters;
+
+import datamodel.buildingblocks.Document;
+
+public class MarkdownExporter {
+
+	public MarkdownExporter(Document document, String outputFileName) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int export() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ exporters/PdfExporter.java
@@ -0,0 +1,16 @@
+package exporters;
+
+import datamodel.buildingblocks.Document;
+
+public class PdfExporter {
+
+	public PdfExporter(Document document, String outputFileName) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int export() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
